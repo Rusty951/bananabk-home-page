207ac36aa0eae1b1ba335e1935285c282b7da59c
Rusty951 <selfsky21@gmail.com>
2026-04-14 21:49:54 +0900

    fix: Supabase 프로젝트 ID 오타 수정 (bjx→pix) + get-works-content verify_jwt 추가

--- public-config.js
+++ public-config.js
@@ -1,5 +1,5 @@
 window.BANANABK_PUBLIC_CONFIG = window.BANANABK_PUBLIC_CONFIG || {
-    supabaseUrl: 'https://gtuwmsynpdbjxmhfytao.supabase.co',
+    supabaseUrl: 'https://gtuwmsynpdpixmhfytao.supabase.co',
     supabaseAnonKey: 'sb_publishable_4EdiZDMjITFbFju-kCgKkQ_9Rq5ReVN',
     functionsBaseUrl: ''
 };
